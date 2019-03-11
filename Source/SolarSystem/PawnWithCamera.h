bf09c4723f0421c52e193c3a848c240fe0e1f9df
supersaiyansubtlety <supersaiyansubtlety@users.noreply.github.com>
2019-03-10 22:01:01 -0700

    Lerp working

---
+++ Source/SolarSystem/PawnWithCamera.h
@@ -0,0 +1,52 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "Camera/CameraComponent.h"
+#include "Components/SphereComponent.h"
+#include "PawnWithCamera.generated.h"
+
+UCLASS()
+class SOLARSYSTEM_API APawnWithCamera : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	APawnWithCamera();
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void Tick( float DeltaSeconds ) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* InputComponent) override;
+    
+protected:
+    UCameraComponent* OurCamera;
+    
+    //Input variables to store the data we receive from input mappings
+    FVector2D MovementInput;
+    FVector2D CameraInput;
+    UPROPERTY(EditAnywhere, Category = "-Movement")
+    float speed;
+    //lerp stuff
+    float lerpAlpha;
+    FVector spawnLoc;
+    FVector spawnOffset;
+    FRotator camForwardRot;
+    FRotator newCamForwardRot;
+
+    
+    //Input functions
+    void MoveBackward(float AxisValue);
+    void MoveRight(float AxisValue);
+    void PitchCamera(float AxisValue);
+    void YawCamera(float AxisValue);
+    void Bearing();
+	
+};
